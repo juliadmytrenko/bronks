14426edad645d5be3867af92a26f77d88cdaee40
dmytrenkojulia <dmytrenkojulia@gmail.com>
2020-09-05 13:34:20 +0200

    feat: add server

---
+++ server/src/index.js
@@ -0,0 +1,28 @@
+const { GraphQLServer } = require("graphql-yoga");
+const { PrismaClient } = require("@prisma/client");
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
+const Link = require("./resolvers/Link");
+
+const prisma = new PrismaClient();
+
+const resolvers = {
+  Query,
+  Mutation,
+  User,
+  Link,
+};
+
+const server = new GraphQLServer({
+  typeDefs: "./src/schema.graphql",
+  resolvers,
+  context: (request) => {
+    return {
+      ...request,
+      prisma,
+    };
+  },
+});
+
+server.start(() => console.log(`Server is running on http://localhost:4000`));
